c1bdd6197d023d785cd1a8569d6f521ed4690adb
MarkusLinV <maksim.lin.00@mail.ru>
2024-06-17 19:18:31 +0200

    готово

---
+++ com.eczample.uni/model/impl/Teacher.java
@@ -0,0 +1,17 @@
+package model.impl;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import model.User;
+import model.DB.DataBase;
+
+public class Teacher extends User{
+
+    public final static List<Integer> groups = new ArrayList<>();
+    
+    public Teacher(String name, String lastName) {
+        super(name, lastName);
+        this.id = DataBase.teachersDB.size() + 1;
+    }
+}
